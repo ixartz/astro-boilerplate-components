d838203c16aa7b2909bd5c3ed58277497cfe175b
Rémi Weng <ixartz@users.noreply.github.com>
2022-05-16 19:23:00 +0200

    feat: add gap in logo and menu

--- src/components/NavbarTwoColumns.tsx
+++ src/components/NavbarTwoColumns.tsx
@@ -5,7 +5,7 @@ type INavbarProps = {
 };
 
 const NavbarTwoColumns = (props: INavbarProps) => (
-  <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between">
+  <div className="flex flex-col gap-y-3 sm:flex-row sm:items-center sm:justify-between">
     {props.children}
   </div>
 );

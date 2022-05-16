b6fd86d402d719fb9b32c725b7646c039c3f64f4
Rémi Weng <ixartz@users.noreply.github.com>
2022-05-16 21:58:19 +0200

    refactor: remove link in Logo and make varaible

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,13 +6,11 @@ type ILogoProps = {
 };
 
 const Logo = (props: ILogoProps) => (
-  <a href="/">
-    <div className="flex items-center bg-gradient-to-br from-sky-500 to-cyan-400 bg-clip-text text-xl font-bold text-transparent">
-      {props.icon}
+  <div className="flex items-center bg-gradient-to-br from-sky-500 to-cyan-400 bg-clip-text text-xl font-bold text-transparent">
+    {props.icon}
 
-      {props.name}
-    </div>
-  </a>
+    {props.name}
+  </div>
 );
 
 export { Logo };
